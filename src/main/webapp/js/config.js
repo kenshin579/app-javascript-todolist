9e7f7cc5f73dc5f9da613fcaec03be413201ea00
kenshin579 <kenshin579@hotmail.com>
2014-03-13 20:42:19 +0900

    refactoring 작업중

---
+++ src/main/webapp/js/config.js
@@ -0,0 +1,9 @@
+require.config({
+    "baseUrl": "../",
+    "paths": {
+        "domReady": "libs/domReady/domReady",
+        "jquery": "libs/jquery/jquery",
+        "Todo": "js/Todo"
+    }
+});
+
