ea3767cd8e329ae735a5fe58f3aba364cf6ae969
kenshin579 <kenshin579@hotmail.com>
2014-04-03 20:24:27 +0900

    단위테스트 간단한 실행완료

--- src/main/webapp/test/spec/TodoTest.js
+++ src/main/webapp/test/spec/TodoTest.js
@@ -1,11 +1,15 @@
 define([
-], function () {
-    console.log(jasmine.getFixtures().fixturesPath);
+    "Todo"
+], function (Todo) {
+//    console.log(jasmine.getFixtures().fixturesPath);
     jasmine.getFixtures().fixturesPath = "/";
 
     describe("Todo Suite", function () {
         it("데이터를 제대로 load하는지 확인", function () {
-            loadFixtures("index.html");
+            loadFixtures("todo.html");
+
+            var todo = new Todo();
+            todo.loadList();
             expect(true).toBe(true);
         });
     });
